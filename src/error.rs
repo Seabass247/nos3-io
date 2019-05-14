c98a3d34bd6911daaf666237eda78961e78afa6b
Seabass247 <sebjfk@gmail.com>
2019-05-14 19:12:53 -0400

    initial working version (only forwards, receiving is WIP)

---
+++ src/error.rs
@@ -0,0 +1,66 @@
+//
+// Copyright (C) 2018 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use failure::Fail;
+use std::error::Error;
+
+/// Custom errors for UART actions
+#[derive(Fail, Debug, Clone, PartialEq)]
+pub enum UartError {
+    /// Catch-all error case
+    #[fail(display = "Generic Error")]
+    GenericError,
+    /// A read/write call was made while another call was already in-progress
+    #[fail(display = "Serial port already in-use")]
+    PortBusy,
+    /// An I/O error was thrown by the kernel
+    #[fail(display = "IO Error: {}", description)]
+    IoError {
+        /// The underlying error type
+        cause: std::io::ErrorKind,
+        /// Error description
+        description: String,
+    },
+    /// An error was thrown by the serial driver
+    #[fail(display = "Serial Error: {}", description)]
+    SerialError {
+        /// The underlying error type
+        cause: serial::ErrorKind,
+        /// Error description
+        description: String,
+    },
+}
+
+impl From<std::io::Error> for UartError {
+    fn from(error: std::io::Error) -> Self {
+        UartError::IoError {
+            cause: error.kind(),
+            description: error.description().to_owned(),
+        }
+    }
+}
+
+impl From<serial::Error> for UartError {
+    fn from(error: serial::Error) -> Self {
+        UartError::SerialError {
+            cause: error.kind(),
+            description: error.description().to_owned(),
+        }
+    }
+}
+
+/// Errors that occur while reading from and writing to stream
+pub type UartResult<T> = Result<T, UartError>;
---
+++ src/main.rs
@@ -0,0 +1,172 @@
+extern crate serial;
+
+use nosengine_rust::client::uart::*;
+use serial::prelude::*;
+use serial::unix::TTYPort;
+use serial::SystemPort;
+use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::io::ErrorKind;
+use std::path::Path;
+use std::thread;
+use std::sync::mpsc::{self, TryRecvError, Sender, Receiver};
+use std::time::Duration;
+use toml;
+
+const CHAR_SIZE: serial::CharSize = serial::Bits8;
+const PARITY: serial::Parity = serial::ParityNone;
+const STOP_BITS: serial::StopBits = serial::Stop1;
+const FLOW_CONTROL: serial::FlowControl = serial::FlowNone;
+const TIMEOUT: Duration = Duration::from_millis(60);
+
+fn main() {
+    //let mut port = TTYPort::open(Path::new("/dev/ttyUSB0")).unwrap();
+    //port.set_timeout(Duration::from_millis(100));
+
+    let mut config = (include_str!("./config.toml"))
+        .parse::<toml::Value>()
+        .unwrap()
+        .try_into::<toml::value::Table>()
+        .unwrap();
+
+    let mut uart_table = config
+        .remove("uart")
+        .ok_or(std::io::Error::new(
+            ErrorKind::Other,
+            "Error parsing config.toml",
+        ))
+        .unwrap()
+        .try_into::<toml::value::Table>()
+        .unwrap();
+
+    let mut i2c_table = config
+        .remove("i2c")
+        .ok_or(std::io::Error::new(
+            ErrorKind::Other,
+            "Error parsing config.toml",
+        ))
+        .unwrap()
+        .try_into::<toml::value::Table>()
+        .unwrap();
+
+    let mut uart_serial_port = uart_table
+        .remove("serial_port")
+        .ok_or(std::io::Error::new(
+            ErrorKind::Other,
+            "Error parsing config.toml",
+        ))
+        .unwrap()
+        .try_into::<String>()
+        .unwrap();
+    
+    let i2c_adapter_device = i2c_table
+        .remove("adapter_device")
+        .ok_or(std::io::Error::new(
+            ErrorKind::Other,
+            "Error parsing config.toml",
+        ))
+        .unwrap()
+        .try_into::<String>()
+        .unwrap();
+
+    println!(
+        "Config: UART set to '{}', I2C to '{}'\ntype 'help' for commands...",
+        &uart_serial_port, &i2c_adapter_device,
+    );
+
+    // TODO: support multiple instances of uart forwarding threads; make Vec of this
+    let mut uart_tx: Option<mpsc::Sender<()>> = None;
+    
+    loop {
+        let mut input = String::new();
+        print!(">");
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => match input.split_whitespace().next() {
+                Some(cmd) => match cmd {
+                    "help" => {
+                        println!(
+                            "commands: 'uart', or 'i2c', or 'stop'"
+                        );
+                    }
+                    "uart" => {
+                        println!(
+                            "Starting UART forwarding and receiving,{}",
+                            "\nif the program hangs, it's probably a NOS issue, restart NOS3. 
+                        ");
+                        let (tx, rx) = mpsc::channel();
+                        let serial_port = uart_serial_port.clone();
+                        uart_tx = Some(tx);
+                        thread::spawn(move || {
+                            uart_init(serial_port, rx);                
+                        });
+                    }
+                    "i2c" => {
+                        println!("not yet implemented!");
+                    }
+                    "stop" => {
+                        if let Some(tx) = &uart_tx {
+                            tx.send(());
+                        }
+                    }
+                    _ => {
+                        println!("unknown command! try 'help'");
+                    }
+                },
+                None => {
+                    println!("unknown command! try 'help'");
+                }
+            },
+            Err(error) => println!("error: {}", error),
+        }
+    }
+}
+
+fn uart_init(uart_serial_port: String, rx: mpsc::Receiver<()>) {
+
+        let mut uart = match UART::new("fsw", "tcp://localhost:12000", "usart_1", 1) {
+            Ok(uart) => uart,
+            Err(e) => {
+                println!("NOS connection failure. Try restarting NOS3");
+                return;
+            }
+        };
+        //println!("Established NOS connection! Forwarding to '{}'...", &serial);
+        let mut port = serial::open(uart_serial_port.as_str()).unwrap();
+        port.set_timeout(Duration::from_millis(100));
+        let settings = serial::PortSettings {
+            baud_rate: serial::BaudRate::Baud9600,
+            char_size: CHAR_SIZE,
+            parity: PARITY,
+            stop_bits: STOP_BITS,
+            flow_control: FLOW_CONTROL,
+        };
+        port.configure(&settings).unwrap();
+
+        uart.set_callback(move |data: &[u8]| {
+            println!("[ DEBUG ] UART data out");
+            port.write_all(data);
+        });
+
+        loop {
+            match rx.try_recv() {
+                    Ok(_) | Err(mpsc::TryRecvError::Disconnected) => {
+                        println!("UART Stopped.");
+                        break;
+                    },
+                    Err(mpsc::TryRecvError::Empty) => {
+                        //let mut buf = Vec::new();
+                        //let mut in_port = serial::open(uart_serial_port.as_str()).unwrap();
+                        //match in_port.read(buf.as_mut_slice()) {
+                        //    Ok(n) => {
+                                //uart.write(buf.as_slice());
+                        //        println!("[ DEBUG ] UART data in");
+                        //    }
+                        //    Err(err) => {}                            
+                        //}
+                    }
+            }
+        }
+
+}
+
